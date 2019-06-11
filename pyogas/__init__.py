ea1c906ad12426a06a934e84ff9b801bbfb7985e
Morgan Williams <morgan.j.williams@hotmail.com>
2019-06-11 09:52:12 +0800

    Add minimal docs

---
+++ pyogas/__init__.py
@@ -0,0 +1,20 @@
+r"""
+Methods for generating xml-based plot templates for use in
+`IoGas™ <https://reflexnow.com/iogas/>`__ .
+"""
+import sys
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.captureWarnings(True)
+
+from ._version import get_versions
+from .writer.mpl2iogas import (
+    contours_to_FreeXYDiagram,
+    contours_to_GeochemXYDiagram,
+    contours_to_FreeTernaryDiagram,
+)
+__version__ = get_versions()["version"]
+del get_versions
+
+__all__ = ["contours_to_FreeXYDiagram", "contours_to_GeochemXYDiagram"]
