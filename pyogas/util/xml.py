70b70a0c00d72399133f18780fd933fc746e8af4
Morgan Williams <morgan.j.williams@hotmail.com>
2019-07-17 11:39:31 +0800

    Update XML printing from lxml

--- pyogas/util/xml.py
+++ pyogas/util/xml.py
@@ -1,12 +1,21 @@
-from xml.etree.ElementTree import tostring
-from xml.etree import ElementTree
+from lxml import etree as ET
+from xml.etree.ElementTree import Element
 from xml.dom import minidom
+import re
 
 
-def prettify_xml(elem):
+def prettify_xml(elem, encoding="utf-8"):
     """
     Return a pretty-printed XML string for an Element.
+
+    Todo
+    ------
+
     """
-    rough_string = ElementTree.tostring(elem, "utf-8")
+    if isinstance(elem, (Element, ET._Element)):
+        rough_string = ET.tostring(elem, encoding=encoding)
+    else:
+        rough_string = ET.tostring(ET.fromstring(elem), encoding=encoding)
     reparsed = minidom.parseString(rough_string)
-    return reparsed.toprettyxml(indent="  ")
+    pattern = re.compile(">([.^<>\n]*?)(\s?)<", re.DOTALL)
+    return pattern.sub(">\g<2><", reparsed.toprettyxml(indent="  "))
